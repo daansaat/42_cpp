6c791a6d983f9dd6faef05c94edd7b16870a24b4
Daan Saat <daansaat@Daans-MacBook-Air.local>
2022-09-25 16:24:40 +0200

    cleanup

--- module_00/ex01/Phonebook.class.cpp
+++ module_00/ex01/Phonebook.class.cpp
@@ -3,6 +3,24 @@
 #include <iostream>
 #include <iomanip>
 
+static std::string	truncate(std::string str) {
+	if (str.length() > 10)
+		return (str.substr(0, 9) + ".");
+	return (str);
+}
+
+static void	print(std::string field) {
+	std::cout << std::setw(10) << truncate(field) << " | ";
+}
+
+static void	print(int index) {
+	std::cout << std::setw(10) << index << " | ";
+}
+
+static void	print(std::string field, const char *str) {
+	std::cout << str << field << std::endl;
+}
+
 void	Phonebook::add(int index) {
 	Contact	contact;
 
@@ -10,20 +28,15 @@ void	Phonebook::add(int index) {
 	this->contact[index] = contact;
 }
 
-static std::string	truncate(std::string str) {
-	if (str.length() > 10)
-		return (str.substr(0, 9) + ".");
-	return (str);
-}
-
 bool	Phonebook::search(void) {
 	int	i;
 	
 	for (i = 0; !contact[i].get_fn().empty() && i < 8; i++) {
-		std::cout << std::setw(10) << i + 1 << " | ";
-		std::cout << std::setw(10) << truncate(contact[i].get_fn()) << " | ";
-		std::cout << std::setw(10) << truncate(contact[i].get_ln()) << " | ";
-		std::cout << std::setw(10) << truncate(contact[i].get_nn()) << std::endl;
+		print(i + 1);
+		print(contact[i].get_fn());
+		print(contact[i].get_ln());
+		print(contact[i].get_nn()); 
+		std::cout << std::endl;
 	}
 	if (i == 0)
 		return (false);
@@ -34,11 +47,11 @@ bool	Phonebook::search(int index) {
 	if (contact[index].get_fn().empty())
 		return (false);
 	else {
-		std::cout << "First name: " << contact[index].get_fn() << std::endl;
-		std::cout << "Last name: " << contact[index].get_ln() << std::endl;
-		std::cout << "Nickname: " << contact[index].get_nn() << std::endl;
-		std::cout << "Phonenumber: " << contact[index].get_pn() << std::endl;
-		std::cout << "Darkest Secret: " << contact[index].get_ds() << std::endl;
+		print(contact[index].get_fn(), "First name: ");
+		print(contact[index].get_ln(), "Last name: ");
+		print(contact[index].get_nn(), "Nickname: ");
+		print(contact[index].get_pn(), "Phonenumber: ");
+		print(contact[index].get_ds(), "Darkest secret");
 	}
 	return (true);
 }
