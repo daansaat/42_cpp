8e8a3811ffa29cfd5e322f6b4d7238666d12221e
Daniel Saat <dsaat@f0r6s8.codam.nl>
2022-10-13 15:24:17 +0200

    now

--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -1,12 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dsaat <dsaat@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/13 13:54:59 by dsaat         #+#    #+#                 */
+/*   Updated: 2022/10/13 13:55:24 by dsaat         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FIXED_HPP
 # define FIXED_HPP
 # include <iostream>
 # include <cmath>
 
 class 	Fixed {
-	
-	int					_fixedNumber;
-	static const int	_fractionalBitsNumber = 8;
 
 	public:
 		Fixed();
@@ -20,6 +29,10 @@ class 	Fixed {
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
 		int		toInt(void) const;
+		
+	private:
+		int					_fixedNumber;
+		static const int	_fractionalBitsNumber = 8;
 };
 
 std::ostream &operator<<(std::ostream &os, const Fixed &obj);
--- module_02/ex02/Fixed.hpp
+++ module_02/ex02/Fixed.hpp
@@ -5,29 +5,36 @@
 /*                                                     +:+                    */
 /*   By: dsaat <dsaat@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/13 13:34:42 by dsaat         #+#    #+#                 */
-/*   Updated: 2022/10/13 13:39:26 by dsaat         ########   odam.nl         */
+/*   Created: 2022/10/13 13:54:59 by dsaat         #+#    #+#                 */
+/*   Updated: 2022/10/13 15:16:48 by dsaat         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 # include <iostream>
+# include <cmath>
 
-class	Fixed {
-	
+class 	Fixed {
+
 	public:
 		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
 		Fixed(const Fixed &obj);
 		Fixed &operator=(const Fixed &obj);
 		~Fixed();
 		
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 		
 	private:
 		int					_fixedNumber;
-		static const int	_fbNumber = 8;
+		static const int	_fractionalBitsNumber = 8;
 };
+
+std::ostream &operator<<(std::ostream &os, const Fixed &obj);
 
 #endif
